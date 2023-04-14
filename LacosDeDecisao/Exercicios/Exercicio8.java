dc937c46137fb1249f537ce3ce79ab382c1e3e2e
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-04-14 13:39:45 -0300

    Exercicio8

--- LacosDeDecisao/Exercicios/Exercicio8.java
+++ LacosDeDecisao/Exercicios/Exercicio8.java
@@ -1,15 +1,38 @@
-package LacosDeDecisao.Exercicios;
-
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercicio8 {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        float saldo = 1000.0f;
 
-        
+        System.out.print("Operação (1 - Saldo, 2 - Saque, 3 - Depósito): ");
+        int operacao = input.nextInt();
 
-        sc.close();
+        switch (operacao) {
+            case 1:
+                System.out.println("Operação - Saldo");
+                System.out.printf("Saldo: R$ %.2f", saldo);
+                break;
+            case 2:
+                System.out.println("Operação - Saque");
+                System.out.print("Valor: R$ ");
+                float saque = input.nextFloat();
+                if (saque <= saldo) {
+                    saldo -= saque;
+                    System.out.printf("Novo Saldo: R$ %.2f", saldo);
+                } else {
+                    System.out.println("Saldo Insuficiente!");
+                }
+                break;
+            case 3:
+                System.out.println("Operação - Depósito");
+                System.out.print("Valor: R$ ");
+                float deposito = input.nextFloat();
+                saldo += deposito;
+                System.out.printf("Novo Saldo: R$ %.2f", saldo);
+                break;
+            default:
+                System.out.println("Operação Inválida!");
+        }
     }
 }
