0185f06b9089efdc6846c1ee18acfd1f0b93b980
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-04-17 13:55:47 -0300

    adding exercises done on repetition links

---
+++ LacosDeRepeticao/Exercicios/E5.java
@@ -0,0 +1,41 @@
+package LacosDeRepeticao.Exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+//Escreva um algoritmo em Java, que leia números inteiros via teclado,
+// até que o número zero seja digitado. Ao final,
+// mostre na tela a soma de todos os números digitados, que sejam positivos
+
+//Na construção do Algoritmo, utilize os seguintes conteúdos:
+//Entrada e Saída de dados
+//Operadores
+//Laços Condicionais
+//Laço de Repetição DO...WHILE
+
+public class E5 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int soma = 0;
+
+        System.out.print("Digite um numero inteiro: ");
+        int numero = sc.nextInt();
+        if (numero > 0) {
+            soma = soma + numero;
+        }
+
+        do {
+            System.out.print("Digite um numero inteiro: ");
+            numero = sc.nextInt();
+            if (numero > 0) {
+                soma = soma + numero;
+            }
+        } while (numero != 0);
+
+        System.out.println(soma);
+
+        sc.close();
+    }
+}
---
+++ LacosDeRepeticao/Exercicios/E6.java
@@ -0,0 +1,45 @@
+package LacosDeRepeticao.Exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+// Escreva um algoritmo em Java,
+// que leia números inteiros via teclado, até que o número zero seja digitado. Ao final,
+// mostre na tela a média de todos os números digitados, que sejam múltiplos de 3.
+
+
+//Na construção do Algoritmo, utilize os seguintes conteúdos:
+//Entrada e Saída de dados
+//Operadores
+//Laços Condicionais
+//Laço de Repetição DO...WHILE
+
+public class E6 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int numero, soma = 0, contador = 0;
+        double media;
+
+        do {
+            System.out.print("Digite um número: ");
+            numero = sc.nextInt();
+
+            if (numero % 3 == 0 && numero != 0) {
+                soma += numero;
+                contador++;
+            }
+        } while (numero != 0);
+
+        if (contador == 0) {
+            System.out.println("Não foram digitados números múltiplos de 3.");
+        } else {
+            media = (double) soma / contador;
+            System.out.println("A média de todos os números múltiplos de 3 é: " + media);
+        }
+
+
+        sc.close();
+    }
+}
