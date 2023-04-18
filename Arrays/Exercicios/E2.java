0a4fcd49e6c8c3d662f701bf8f092dcd930db71b
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-04-18 13:24:03 -0300

    exercises on matrix and array generation

---
+++ Arrays/Exercicios/E2.java
@@ -0,0 +1,63 @@
+package Arrays.Exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+//Elabore um algoritmo que leia 10 números inteiros e armazene em um vetor. Em seguida, mostre na tela:
+//Todos os elementos dos índices ímpares do vetor
+//Todos os elementos do vetor que são números pares
+//A Soma de todos os elementos do vetor
+//A Média de todos os elementos do vetor, armazenada em uma variável do tipo real
+
+public class E2 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        // Criando o vetor
+        final int tamanhoVetor = 10;
+        int[] vetor = new int[tamanhoVetor];
+
+        // Instanciando valores para as posicoes do vetor
+        int soma = 0;
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.printf("Digite a %dª posicao do vetor: ", i + 1);
+            vetor[i] = sc.nextInt();
+            soma += vetor[i];
+        }
+
+        int indiceImpar = 0;
+        // Todos os elementos dos índices ímpares do vetor
+        System.out.println("\nElementos nos índices ímpares: ");
+        for (int i = 0; i < vetor.length; i++) {
+            if (i % 2 != 0) {
+                indiceImpar = vetor[i];
+                System.out.print(indiceImpar + " ");
+            }
+        }
+
+        //Todos os elementos do vetor que são números pares
+        System.out.println("\n\nElementos pares: ");
+        int contadorPar = 0;
+        for (int i = 0; i < vetor.length; i++) {
+            if (vetor[i] % 2 == 0) {
+                contadorPar = vetor[i];
+                System.out.print(contadorPar + " ");
+            }
+        }
+
+        /* A Soma de todos os elementos do vetor
+        int soma = 0;
+        for (int i = 0; i < vetor.length; i++) {
+            soma += vetor[i];
+        }*/
+
+        System.out.print("\n\nSoma: " + soma);
+
+        // A Média de todos os elementos do vetor, armazenada em uma variável do tipo real
+        double media = (double) soma / vetor.length;
+        System.out.printf("\nMédia: %.2f%n", media);
+
+        sc.close();
+    }
+}
---
+++ Arrays/Exercicios/E3.java
@@ -0,0 +1,57 @@
+package Arrays.Exercicios;
+
+//Elabore um algoritmo que leia uma Matriz 3x3 de números inteiros e em seguida, mostre na tela:
+//Todos os elementos da Diagonal Principal
+//Todos os elementos da Diagonal Secundária
+//A Soma de todos os elementos da Diagonal Principal
+//A Soma de todos os elementos da Diagonal Secundária
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class E3 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        // Criando variaveis
+        final int tamanhoMatriz = 3;
+        int diagonalPrincipal = 0, diagonalSecundaria = 0;
+
+        // Criando matriz 3x3
+        int[][] matriz = new int[tamanhoMatriz][tamanhoMatriz];
+
+        //Inserindo dados nos indices da matriz
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.printf("Digite os dados do indice [%d,%d]: ", i, j);
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        //Atribuindo valor a somaDiagonalPrincipal
+        int somaDiagonalPrincipal = 0;
+        //Atribuindo valor a somaDiagonalSecundaria
+        int somaDiagonalSecundaria = 0;
+
+        // Mostrando elementos da diagonal principal
+        System.out.println("\nElementos da diagonal principal:");
+        for (int i = 0; i < tamanhoMatriz; i++) {
+            System.out.print(matriz[i][i] + " ");
+            somaDiagonalPrincipal += matriz[i][i];
+        }
+
+        // Mostrando elementos da diagonal secundaria
+        System.out.println("\n\nElementos da diagonal secundária:");
+        for (int i = 0; i < tamanhoMatriz; i++) {
+            System.out.print(matriz[i][2 - 1] + " ");
+            somaDiagonalSecundaria += matriz[i][2 - i];
+        }
+
+        System.out.println("\n\nSoma da diagonal principal: " + somaDiagonalPrincipal);
+        System.out.println("Soma da diagonal secundária: " + somaDiagonalSecundaria);
+
+        sc.close();
+
+    }
+}
